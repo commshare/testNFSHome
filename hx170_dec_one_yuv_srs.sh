e54a99db01ee6db3ba35478067f6e8badc1993f3
zhangbin <commshare.zhang@gmail.com>
2015-08-14 16:00:05 +0800

    -P to output YUV420P ,test ok !

--- hx170_dec_one_yuv_srs.sh
+++ hx170_dec_one_yuv_srs.sh
@@ -2,16 +2,25 @@
 echo "rm srs_1.yuv"
 rm -rf srs_1.yuv
 echo "do test ..."
-CMD=
-if [ $# == 1 ];then
+#CMD=
+#OUT=
+#echo "000000"
+if [ $# -eq 0 ];then
     echo "use default to decode one yuv"
-else
-    if [ $1 == "-P" ];then
+    OUT=srs_1_sp.yuv
+elif [ "$1" = "-P" ];then
         echo "use -P to output planar yuv"
-        CMD=$1
-    fi
+        CMD="$1"
+        OUT=srs_1_p.yuv
+    else
+        echo "no idea for cmd param"
+    
 fi
-    ./hx170dec_versatile -N1 -Osrs_1.yuv $CMD 720p.h264.raw
+    echo "now : CMD=$CMD OUT=$OUT "
+  
+    echo "do decoder test ...."
+    ./hx170dec_versatile -N1 -O$OUT $CMD 720p.h264.raw
+
     
 
 echo "....done....."
